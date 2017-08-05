Finally fixed bug in the input_pipeline (tf.stack function, wrong axis)

tensorflow-reading-pipeline/trainer/eval_listener.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import os
+import time
 
 import trainer.training_util as u
 import trainer.input_pipeline as ip
@@ -20,8 +21,9 @@ class EvalCheckpointSaverListener(tf.train.CheckpointSaverListener):
 
         self._saver = tf.train.Saver()
 
+        current_dt = time.strftime("%Y%m%d-%H%M%S")
         self._file_writer = tf.summary.FileWriter(
-            os.path.join(event_dir, 'eval'), graph=self._graph)
+            os.path.join(event_dir, current_dt + '-eval'), graph=self._graph)
 
         with self._graph.as_default():
             features, labels = ip.input_pipeline(
@@ -31,7 +33,8 @@ class EvalCheckpointSaverListener(tf.train.CheckpointSaverListener):
                 shuffle=False)
 
             m.cnn_model(features, labels, layers_layout, keep_prob=1)
-            self._summary_op = tf.summary.merge_all(u.EVAL_SUMMARY_OP)
+            #self._summary_op = tf.summary.merge_all(u.EVAL_SUMMARY_OP)
+            self._summary_op = tf.summary.merge_all("debug")
 
     def after_save(self,
                   session,

tensorflow-reading-pipeline/trainer/model.py
@@ -73,10 +73,11 @@ def cnn_model(features,
 
         logits = curr_layer  # the last layer output Tensor contains the logits
 
-        pred_op = tf.argmax(logits, axis=1, name="predict")
+        pred_op = tf.argmax(logits, axis=1)
         _add_accuracy_ops(pred_op, labels)
 
         loss_op = _add_loss_op(logits, labels)
+
         train_op = _add_train_op(loss_op, global_step)
 
         # log weights of all trainable variables in the tf event file
@@ -114,8 +115,9 @@ def _add_accuracy_ops(pred, labels):
 
 def _add_loss_op(logits, labels):
     with tf.name_scope("loss"):
-        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels),
-                              name="xent")
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels), name="xent")
+        tf.summary.scalar("loss", loss, collections=["debug"])
+        return loss
 
 
 def _add_train_op(loss, global_step):

tensorflow-reading-pipeline/trainer/task.py
@@ -3,18 +3,26 @@ import trainer.model as m
 import trainer.eval_listener as h
 import tensorflow as tf
 import os
+import time
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 def run(train_filename, eval_filename, layers_layout, chkpt_dir, event_dir):
+
     features, labels = ip.input_pipeline(
         train_filename,
-        num_epochs=2,
+        num_epochs=1,
         batch_size=50,
-        shuffle=True)
+        shuffle=False)
 
-    train_op, global_step = m.cnn_model(features, labels, layers_layout, keep_prob=0.5)
+    train_op, global_step = m.cnn_model(features, labels, layers_layout, keep_prob=1)
+
+    current_dt = time.strftime("%Y%m%d-%H%M%S")
+    file_writer = tf.summary.FileWriter(
+        os.path.join(event_dir, current_dt + '-eval-train'),
+        train_op.graph)
+    summary_op = tf.summary.merge_all("debug")
 
     init_op = tf.group(tf.global_variables_initializer(),
                        tf.local_variables_initializer())
@@ -36,7 +44,12 @@ def run(train_filename, eval_filename, layers_layout, chkpt_dir, event_dir):
 
     with tf.train.SingularMonitoredSession(hooks=[chkpt_hook], scaffold=scaf) as sess:
         while not sess.should_stop():
-            sess.run(train_op)
+            _, summ, gs = sess.run([train_op, summary_op, global_step])
+            file_writer.add_summary(summ, global_step=gs)
+            # fc5, lab = sess.run(["full5/BiasAdd:0", "lab_run:0", train_op])[0:2]
+            # print(fc5.dtype, lab.dtype)
+            # feat = sess.run(["input_pipeline/feat:0", train_op])[0:1]
+            # print(feat)
 
     """
     # Create a session for running operations in the Graph.